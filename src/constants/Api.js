1d56e0292e8840cf0bbf11e6a0e3d200f40b4b12
Jurgis Rudaks <jurgis.rudaks@accenture.com>
2016-08-17 10:10:12 +0300

    API Configurable with env variables

--- src/constants/Api.js
+++ src/constants/Api.js
@@ -1,6 +1,6 @@
-const API_PROTOCOL = 'http'
-const API_HOST = 'localhost'
-const API_PORT = '3001'
-const API_VERSION = '1'
+const API_PROTOCOL = process.env.apiProtocol || 'http'
+const API_HOST = process.env.apiHost || 'localhost'
+const API_PORT = process.env.apiPort || '3001'
+const API_VERSION = process.env.apiVersion || '1'
 
 export const API_ROOT = API_PROTOCOL + '://' + API_HOST + ':' + API_PORT + '/api/' + API_VERSION + '/'
