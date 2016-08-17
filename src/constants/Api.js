5d27e4c521b604e4f96f7a11a92eeb17a921cf24
Jurgis Rudaks <jurgis.rudaks@accenture.com>
2016-08-17 10:22:32 +0300

    Made env variables accessable to React app

--- src/constants/Api.js
+++ src/constants/Api.js
@@ -1,6 +1,6 @@
-const API_PROTOCOL = process.env.apiProtocol || 'http'
-const API_HOST = process.env.apiHost || 'localhost'
-const API_PORT = process.env.apiPort || '3001'
-const API_VERSION = process.env.apiVersion || '1'
+const API_PROTOCOL = process.env.API_PROTOCOL || 'http'
+const API_HOST = process.env.API_HOST || 'localhost'
+const API_PORT = process.env.API_PORT || '3001'
+const API_VERSION = process.env.API_VERSION || '1'
 
 export const API_ROOT = API_PROTOCOL + '://' + API_HOST + ':' + API_PORT + '/api/' + API_VERSION + '/'
